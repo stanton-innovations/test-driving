f4cadf68c617e67c7655e439ae667a1aa650c1b8
dartavion <richardranke@gmail.com>
2018-04-23 17:37:38 -0400

    switchmap

--- src/app/services/tinker.service.ts
+++ src/app/services/tinker.service.ts
@@ -1,14 +1,19 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { pluck, switchMap, toArray } from 'rxjs/internal/operators';
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable()
 export class TinkerService {
 
   constructor(private http: HttpClient) { }
 
   get() {
-    return this.http.get('/api/characters/5').subscribe(data => console.log(data));
+    return this.http.get('/api/characters/5')
+      .pipe(
+        pluck('fiveJson', 'game_indices'),
+        switchMap((data: any) => data),
+        pluck('version', 'name'),
+        toArray()
+      ).subscribe(data => console.log(data));
   }
 }
